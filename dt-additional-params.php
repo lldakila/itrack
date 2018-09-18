0c55ef183458df33133a8b2539f3a282486c2086
Sly Flores <sly@christian.com.ph>
2018-09-18 17:11:33 +0800

    adding document type additional params

---
+++ dt-additional-params.php
@@ -0,0 +1,15 @@
+<?php
+
+define('dt_add_params', array(
+	array(
+		"id"=>1,
+		"document_type"=>2,
+		"description"=>"Travel Orders",
+		"params"=>array(
+			array("id"=>1,"description"=>"","type"=>"select","options"=array()),
+			array("id"=>1,"description"=>"","type"=>"input","value"=>""),
+		),
+	),
+));
+
+?>
