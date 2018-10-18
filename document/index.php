6ed28041684a0771d05c5c2d7ed1c157014359eb
Sly Flores <sly@christian.com.ph>
2018-10-18 17:09:05 +0800

    view document update

--- document/index.php
+++ document/index.php
@@ -34,7 +34,7 @@ $app->get('/view/{id}', function ($request, $response, $args) {
 
 })->setName('document');
 
-$app->get('/info/{id}', function ($request, $response, $args) {
+$app->get('/view/info/{id}', function ($request, $response, $args) {
 
 	$con = $this->con;
 	$con->table = "documents";	
@@ -43,9 +43,31 @@ $app->get('/info/{id}', function ($request, $response, $args) {
 	
 	$id = $args['id'];
 	
-	$document = $con->getData("SELECT id, barcode, doc_name, doc_type, origin, other_origin, communication, document_transaction_type, document_date FROM documents WHERE id = $id");
+	$document = $con->getData("SELECT id, barcode, doc_name, doc_type, origin, other_origin, communication, document_transaction_type, document_date, dt_add_params FROM documents WHERE id = $id");
 	
 	if (count($document)) {
+		
+		# first track
+		$tracks = $con->getData("SELECT * FROM tracks WHERE document_id = ".$document['0']['id']."  ORDER BY system_log");
+		
+		$document[0]['for_initial'] = false;
+		$document[0]['for_signature'] = false;
+		$document[0]['for_routing'] = false;
+
+		$document[0]['document_dt_add_params'] = json_decode($document[0]['dt_add_params'],false);
+		$document[0]['document_action_add_params'] = [];
+		
+		if (count($tracks)) {
+			
+			$first_track = $tracks[0];
+			
+			if ($first_track['track_action']==1) $document[0]['for_initial'] = true;
+			if ($first_track['track_action']==2) $document[0]['for_signature'] = true;
+			if ($first_track['track_action']==3) $document[0]['for_routing'] = true;
+			
+			$document[0]['document_action_add_params'] = json_decode($first_track['track_action_add_params'],false);
+			
+		};
 		
 		$document[0]['document_date_barcode'] = date("M j, Y h:i:s A",strtotime($document[0]['document_date']));
 		$document = document_info_complete($con,$document[0]);
@@ -59,10 +81,6 @@ $app->get('/info/{id}', function ($request, $response, $args) {
 $app->get('/for/initial/{id}', function ($request, $response, $args) {
 
 	require_once '../path_url.php';
-	
-	# query
-	
-	#
 
     return $this->view->render($response, 'initial.html', [
 		'path'=>$base_path,	
