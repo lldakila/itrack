107c4787c3db5d4bd91db164ed8df7b948fc2d38
Sly Flores <sly@christian.com.ph>
2018-10-20 23:09:10 +0800

    open doc via barcode.. done

---
+++ modules/barcode-listener-document.js
@@ -0,0 +1,70 @@
+angular.module('barcode-listener-document',['bootstrap-modal','form-validator-dialog','bootstrap-growl']).directive('listenBarcode', function($document,$rootScope,$http,$window,bootstrapModal,validateDialog,growl,$q) {
+
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+		
+			$(document).scannerDetection({
+				timeBeforeScanTest: 200, // wait for the next character for upto 200ms
+				startChar: [120], // Prefix character for the cabled scanner (OPL6845R)
+				endChar: [13], // be sure the scan is complete if key 13 (enter) is detected
+				avgTimeByChar: 40, // it's not a barcode if a character takes longer than 40ms
+				onComplete: barcode // main callback function	
+			});
+			
+			$document.bind('keydown', function(e) {
+				if ((e.ctrlKey) && (e.which === 13)) {
+					manual_barcode(scope);
+				};
+				// $rootScope.$broadcast('keypress', e);
+				// $rootScope.$broadcast('keypress:' + e.which, e);				
+			});			
+			
+			function manual_barcode(scope) {
+			
+				delete scope.barcode;
+			
+				var onOk = function() {
+					
+					if (validateDialog.form(scope,'barcode')) return false;
+					
+					barcode(scope.barcode,0);
+					
+					return true;
+					
+				};
+			
+				bootstrapModal.box(scope,'Manual Barcode','dialogs/manual-barcode.html',onOk,function() {});
+				
+			};
+			
+			function barcode(barcode, qty) {
+				
+				$http({
+					method: 'GET',
+					url: 'api/documents/barcode/'+barcode,
+				}).then(function success(response) {
+					
+					// check if barcode is valid
+					if (response.data.status) {
+						view_document(response.data.id);
+					} else {
+						growl.show('alert alert-danger no-border mb-2',{from: 'top', amount: 60},'Invalid barcode. No document found.');					
+					};
+					
+				}, function error(response) {				
+				
+				});
+				
+			};
+			
+			function view_document(id) {
+
+				$window.location.href = 'document/view/'+id;
+
+			};
+
+		}
+	};	
+
+});
