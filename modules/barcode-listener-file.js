241402351a93d8761fbe403b87cb7c1043842f08
Sly Flores <sly@christian.com.ph>
2019-01-30 13:59:40 +0800

    1/30/19
    x char is doesn't work with scannerdetection - fixed

--- modules/barcode-listener-file.js
+++ modules/barcode-listener-file.js
@@ -6,8 +6,8 @@ angular.module('barcode-listener-file',['bootstrap-modal','form-validator-dialog
 		
 			$(document).scannerDetection({
 				timeBeforeScanTest: 200, // wait for the next character for upto 200ms
-				startChar: [120], // Prefix character for the cabled scanner (OPL6845R)
-				endChar: [13], // be sure the scan is complete if key 13 (enter) is detected
+				// startChar: [120], // Prefix character for the cabled scanner (OPL6845R)
+				// endChar: [13], // be sure the scan is complete if key 13 (enter) is detected
 				avgTimeByChar: 40, // it's not a barcode if a character takes longer than 40ms
 				onComplete: barcode // main callback function	
 			});
--- modules/barcode-listener-receive.js
+++ modules/barcode-listener-receive.js
@@ -4,10 +4,14 @@ angular.module('barcode-listener-receive',['bootstrap-modal','form-validator-dia
 		restrict: 'A',
 		link: function(scope, element, attrs) {
 		
+			/* $(document).scannerDetection({ // print each character
+				onKeyDetect: function(event){console.log(event.which); return false;}
+			}); */
+		
 			$(document).scannerDetection({
 				timeBeforeScanTest: 200, // wait for the next character for upto 200ms
-				startChar: [120], // Prefix character for the cabled scanner (OPL6845R)
-				endChar: [13], // be sure the scan is complete if key 13 (enter) is detected
+				// startChar: [120], // Prefix character for the cabled scanner (OPL6845R)
+				// endChar: [13], // be sure the scan is complete if key 13 (enter) is detected
 				avgTimeByChar: 40, // it's not a barcode if a character takes longer than 40ms
 				onComplete: barcode // main callback function	
 			});
