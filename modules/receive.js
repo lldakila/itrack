302c8235637e76b904722d4a4515680b75b2515b
Sly Flores <sly@christian.com.ph>
2019-09-02 16:58:59 +0800

    ...

--- modules/receive.js
+++ modules/receive.js
@@ -1,4 +1,4 @@
-angular.module('app-module', ['bootstrap-modal','ui.bootstrap','notifications-module','block-ui','bootstrap-growl','module-access','barcode-listener-receive','receive-document']).factory('app', function($http,$timeout,$compile,$window,bootstrapModal,bui,access,growl,receive) {
+angular.module('app-module', ['bootstrap-modal','ui.bootstrap','notifications-module','block-ui','bootstrap-growl','module-access','barcode-listener-receive','receive-document']).factory('app', function($http,$timeout,$compile,$window,bootstrapModal,bui,access,growl,receive,$q) {
 	
 	function app() {
 
@@ -77,127 +77,165 @@ angular.module('app-module', ['bootstrap-modal','ui.bootstrap','notifications-mo
 			
 		};
 		
-		self.open = function(scope,d) {			
+		self.open = function(scope,d) {	
 			
-			scope.doc = {};
-			scope.doc.id = d.id;
-			scope.doc.actions = [];
-			
-			scope.views.alert = {};
-			scope.views.alert.actions = {};
-			scope.views.alert.actions.show = false;
-			scope.views.alert.actions.message = "";
-			
-			$http({
-				method: 'GET',
-				url: 'document/view/actions/'+d.id
-			}).then(function mySuccess(response) {
+			isReceive(scope,d).then(function success(response) {
 				
-				scope.doc.actions = angular.copy(response.data);
-				checkboxActionParam(scope);
-
-			}, function myError(response) {
-		
-		
-			});
-			
-			/* $http({
-				method: 'GET',
-				url: 'api/receive-document/actions'
-			}).then(function mySuccess(response) {
-				
-				scope.doc.actions = angular.copy(response.data);
-
-			}, function myError(response) {
-		
-		
-			});	 */
-			
-			var onOk = function() {
-				
-				bui.show();
-				
-				scope.views.alert.actions.show = false;
-				scope.views.alert.actions.message = "";			
-				
-				var actions = 'false';
-				var options = 'true';
-
-				var doc_actions = ['for_initial','for_signature','for_routing'];
-
-				doc_actions.forEach(function(action,i) {
+				if (!response) {
 					
-					actions+=(scope.doc.actions[action].value)?'||true':'||false';
-					
-					if (scope.doc.actions[action].value) {
-						
-						options+='&&(';
-						
-						angular.forEach(scope.doc.actions[action].params,function(param,ii) {
-							
-							if (param.type=='checkbox') {
-							
-								angular.forEach(param.options, function(option,iii) {
-									
-									if (iii==0) options+=(option.value)?'true':'false';
-									else options+=(option.value)?'||true':'||false';						
-									
-								});
-							
-							};
-							
-							if (param.type=='select') {
-								
-								options+=(param.value.id>0)?'true':'false';						
-								
-							};
-							
-						});
-						
-						options+=')';
-						
-					};
-					
-				});
-				
-				if (!eval(actions)) {
-					
-					scope.views.alert.actions.show = true;
-					scope.views.alert.actions.message = "Pleas select an action";
-					bui.hide();
-					return false;
+					growl.show('alert alert-danger no-border mb-2',{from: 'top', amount: 60},'Document is not yet received in your office');
 					
 				} else {
 					
-					if (!eval(options)) {
+					open(scope);
+					
+				}
+				
+			}, function error(response) {
+				
+				
+			});
+			
+			function open(scope) {
+				
+				scope.doc = {};
+				scope.doc.id = d.id;
+				scope.doc.actions = [];
+				
+				scope.views.alert = {};
+				scope.views.alert.actions = {};
+				scope.views.alert.actions.show = false;
+				scope.views.alert.actions.message = "";
+				
+				$http({
+					method: 'GET',
+					url: 'document/view/actions/'+d.id
+				}).then(function mySuccess(response) {
+					
+					scope.doc.actions = angular.copy(response.data);
+					checkboxActionParam(scope);
 
+				}, function myError(response) {
+			
+			
+				});
+				
+				var onOk = function() {
+					
+					bui.show();
+					
+					scope.views.alert.actions.show = false;
+					scope.views.alert.actions.message = "";			
+					
+					var actions = 'false';
+					var options = 'true';
+
+					var doc_actions = ['for_initial','for_signature','for_routing'];
+
+					doc_actions.forEach(function(action,i) {
+						
+						actions+=(scope.doc.actions[action].value)?'||true':'||false';
+						
+						if (scope.doc.actions[action].value) {
+							
+							options+='&&(';
+							
+							angular.forEach(scope.doc.actions[action].params,function(param,ii) {
+								
+								if (param.type=='checkbox') {
+								
+									angular.forEach(param.options, function(option,iii) {
+										
+										if (iii==0) options+=(option.value)?'true':'false';
+										else options+=(option.value)?'||true':'||false';						
+										
+									});
+								
+								};
+								
+								if (param.type=='select') {
+									
+									options+=(param.value.id>0)?'true':'false';						
+									
+								};
+								
+							});
+							
+							options+=')';
+							
+						};
+						
+					});
+					
+					if (!eval(actions)) {
+						
 						scope.views.alert.actions.show = true;
-						scope.views.alert.actions.message = "Choice is required in each actions selected";
+						scope.views.alert.actions.message = "Pleas select an action";
 						bui.hide();
 						return false;
 						
+					} else {
+						
+						if (!eval(options)) {
+
+							scope.views.alert.actions.show = true;
+							scope.views.alert.actions.message = "Choice is required in each actions selected";
+							bui.hide();
+							return false;
+							
+						};
+						
 					};
+
+					$http({
+						method: 'POST',
+						url: 'document/doc/office/action',
+						data: scope.doc
+					}).then(function mySuccess(response) {							
+				
+						bui.hide();
+
+					}, function myError(response) {								
+					
+						bui.hide();
+					});
+					
+					return true;
 					
 				};
-
-				$http({
-					method: 'POST',
-					url: 'document/doc/office/action',
-					data: scope.doc
-				}).then(function mySuccess(response) {							
-			
-					bui.hide();
-
-				}, function myError(response) {								
 				
-					bui.hide();
-				});
-				
-				return true;
+				bootstrapModal.box(scope,'Document Actions','/dialogs/actions.html',onOk);
 				
 			};
 			
-			bootstrapModal.box(scope,'Document Actions','/dialogs/actions.html',onOk);
+		};
+		
+		function isReceive(scope,doc) {
+			
+		  return $q(function(resolve, reject) {
+			  
+			$http({
+				method: 'GET',
+				url: 'document/doc/transit/is_receive/'+doc.id,
+			}).then(function mySuccess(response) {							
+		
+				if (response.data==1) {
+					
+					resolve(true);
+					
+				} else {
+
+					resolve(false);
+					
+				}
+	
+			}, function myError(response) {					
+			
+				reject(null);
+
+			});		
+			
+		  });			
 			
 		};
 		
