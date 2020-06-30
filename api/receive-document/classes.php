3234280793ef45bacb45cfd5af19a73dc0f39105
Jp <johnpaulbalanon6@gmail.com>
2020-06-30 10:33:09 +0800

    conflict

--- api/receive-document/classes.php
+++ api/receive-document/classes.php
@@ -1,6 +1,8 @@
 <?php
 
-function uploadFiles($con,$uploads,$barcode,$id,$path=null) {
+function uploadFiles($con,$uploads,$barcode,$id,$path,$init) {
+	
+	$user_id = $_SESSION['itrack_user_id'];
 	
 	$ft = array(
 		"jpeg"=>".jpg",
@@ -8,11 +10,7 @@ function uploadFiles($con,$uploads,$barcode,$id,$path=null) {
 		"png"=>".png"
 	);
 
-	$con->table = "files";	
-
-	# clear files table for document if there are any
-	$con->deleteData(array("document_id"=>$id));
-	#
+	$con->table = "files";
 
 	if (count($uploads["files"])) {
 	
@@ -21,20 +19,33 @@ function uploadFiles($con,$uploads,$barcode,$id,$path=null) {
 		
 		if (!folder_exist($dir)) mkdir($dir);		
 
+		$incr = 0;
+		$files = $con->getData("SELECT * FROM files WHERE document_id = $id AND user_id = $user_id ORDER BY id DESC");
+		if (count($files)) {
+			$file_and_ext = explode(".",$files[0]['file_name']);
+			$name_index = explode("_",$file_and_ext[0]);
+			$incr = intval($name_index[1]);
+		};
+
 		foreach ($uploads["files"] as $key => $f) {
 
 			if ($f['file'] == "") continue;
+			if (isset($f['initial_file'])) if ($f['initial_file']>0) continue;
 
 			$imgData = str_replace(' ','+',$f['file']);
 			$imgData =  substr($imgData,strpos($imgData,",")+1);
 			$imgData = base64_decode($imgData);
-			$fileName = "$barcode"."_$key".$ft[$f['type']];
+			
+			$incr++;
+
+			$fileName = "$barcode"."_$incr".$ft[$f['type']];
 			$filePath = "$dir/$fileName";
 			$file = fopen($filePath, 'w');
 			fwrite($file, $imgData);
 			fclose($file);
-
-			$data = array("document_id"=>$id,"file_name"=>$fileName);
+			
+			$data = array("document_id"=>$id,"file_name"=>$fileName,"user_id"=>$user_id);
+			if ($init) $data['initial_file'] = 1;
 			$con->insertData($data);
 
 		};
@@ -45,41 +56,79 @@ function uploadFiles($con,$uploads,$barcode,$id,$path=null) {
 
 function deleteFiles($con,$files,$path=null) {
 	
+	$table = $con->table;
+	$con->table = "files";
+	
 	$dir = "../../files";
 	if ($path != null) $dir = $path;
 	
 	foreach ($files as $file) {
 		
-		if (file_exists($dir."/".$file)) unlink($dir."/".$file);	
+		if (file_exists($dir."/".$file['file_name'])) unlink($dir."/".$file['file_name']);
+		$delete = $con->deleteData(array("id"=>$file['id']));
 		
 	};
 	
+	$con->table = $table;
+	
 };
 
-function barcode($con,$origin,$office,$com) {
+function barcode($con,$params) {
 	
 	$barcode = "";
 	
-	$incr = 1;
+	$series = 1;
 	
-	$current_year = date("Y");
-	$sql = "SELECT documents.id, documents.barcode FROM documents WHERE documents.origin = $origin AND barcode LIKE '%-$current_year-%' ORDER BY documents.id DESC LIMIT 1";
+	$current_year = date("Y");	
+	$sql = "SELECT documents.id, documents.barcode, documents.doctype_series FROM documents WHERE documents.barcode LIKE '%-".$current_year."-%' AND  documents.origin = ".$params['origin']." AND documents.doc_type = ".$params['doctype']." ORDER BY documents.doctype_series DESC LIMIT 1";
 
 	$last_barcode = $con->getData($sql);	
 	
 	if (count($last_barcode)) {
-
-		$last_gen = explode("-",$last_barcode[0]['barcode']);		
 		
-		$incr = (isset($last_gen[3]))?(int)$last_gen[3]:0;
+		$series = $last_barcode[0]['doctype_series'];
 		
-		$incr+=1;
+		$series += 1;
 		
 	};
 	
-	$barcode = substr($office,0,3)."-".date("m")."-".date("Y")."-".str_pad($incr, 5, '0', STR_PAD_LEFT);
+	// $barcode = substr($params['office'],0,3)."-".date("m")."-".date("Y")."-".str_pad($series, 5, '0', STR_PAD_LEFT);
+	$barcode = $params['office']."-".$params['doctype_shortname']."-".date("Y")."-".str_pad($series, 5, '0', STR_PAD_LEFT);
 	
-	return $barcode;
+	// check for duplicancy
+	$check = $con->getData("SELECT * FROM documents WHERE barcode = '$barcode'");	
+	while (count($check)) {
+		
+		$new_barcode = resolve_duplicate_barcode($barcode,$series);
+		$barcode = $new_barcode['barcode'];
+		$series = $new_barcode['series'];
+		
+		$check = $con->getData("SELECT * FROM documents WHERE barcode = '$barcode'");		
+		
+	}; // until unique barcode is generated
+	
+	$response = array(
+		"barcode"=>$barcode,
+		"series"=>$series
+	);
+	
+	return $response;
+	
+};
+
+function resolve_duplicate_barcode($barcode,$series) {
+	
+	$extracts = explode("-",$barcode);	
+	
+	$new_series = $series + 1;
+	$new_barcode = $extracts[0]."-".$extracts[1]."-".$extracts[2]."-".str_pad($new_series, 5, '0', STR_PAD_LEFT);
+	
+	$response = array(
+		"barcode"=>$new_barcode,
+		"series"=>$new_series
+	);
+	
+	return $response;	
 	
 };
 
